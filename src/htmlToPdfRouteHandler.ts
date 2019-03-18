ff65e1138c16eb01cdbdb66019a9029348531a98
Pavol Hejny <me@pavolhejny.com>
2019-03-18 10:44:47 +0100

    Cleanup imports

--- src/htmlToPdfRouteHandler.ts
+++ src/htmlToPdfRouteHandler.ts
@@ -1,13 +1,7 @@
-import { s3 } from './storage';
-import * as path from 'path';
 import { RequestHandler } from 'express';
-import { execute } from './tools/execute';
-import { createHash } from 'crypto';
-import { parse } from 'url';
-import { readFileSync, unlinkSync } from 'fs';
-import { CACHE_DIR, ERROR_WRONG_URL, AWS_S3_BUCKET_NAME } from './config';
+import { ERROR_WRONG_URL } from './config';
 import { getConvertedFile } from './getConvertedFile';
-const slimerJS = require('slimerjs');
+
 
 export const htmlToPdfRouteHandler: RequestHandler = async (
     { query: { url, download, nocache } },
