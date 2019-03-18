0f02455f5060fac378cfd0411decbd8298d4327a
Pavol Hejny <me@pavolhejny.com>
2019-03-18 11:04:13 +0100

    Front of conversions

--- src/preconvert.ts
+++ src/preconvert.ts
@@ -1,26 +1,40 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import * as path from 'path';
 import { getConvertedFile } from './getConvertedFile';
+
+const preconvertFilename = path.join(__dirname, '../preconvert.txt');
 
 preconvert();
 
 async function preconvert() {
-    const preconvertText = readFileSync(
-        path.join(__dirname, '../preconvert.txt'),
-    ).toString();
-    const urls = preconvertText
-        .split('\n')
-        .map((url) => url.trim())
-        .filter((url) => url !== '');
 
-    let i = 0;
-    for (const url of urls) {
-        i++;
-        console.log(`[ ${i} / ${urls.length} ] Converting "${url}"`);
-        try {
-            await getConvertedFile(url);
-        } catch (error) {
-            console.log(`Error while converting "${url}".`);
-        }
+    const preconvertText = readFileSync(preconvertFilename).toString();
+
+    const urls = preconvertText
+    .split('\n')
+    .map((url) => url.trim())
+    .filter((url) => url !== '');
+
+    if(urls.length===0){
+        console.log(`"preconvert.txt" file is empty! You can now safely terminate the script.`);
+        setInterval(()=>{
+            //this is just a hack for keeping the script running
+        },1000);
+        return;
     }
+
+    const [ url ] = urls.splice(0,1);
+
+    console.log(`[ ${urls.length} left ] Converting "${url}"`);
+    try {
+        await getConvertedFile(url);
+    } catch (error) {
+        console.log(`Error while converting "${url}".`);
+    }
+
+    const preconvertTextAppex = urls.join('\n');
+    writeFileSync(preconvertFilename,preconvertTextAppex);
+
+    
+    preconvert();//todo is this tail recursion memory optimal?
 }
