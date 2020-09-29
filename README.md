69f2f009e5dc51aee8ce486f5c7cb788cb945867
Pavol Hejny <me@pavolhejny.com>
2020-09-29 15:38:03 +0200

    Google cloud

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PDF Maker
 
 Look at https://www.pavolhejny.com/pdf-maker
+
+
+## Deploy
+
+You need to install Google cloud SDK
---
+++ distx/index.js
@@ -0,0 +1,23 @@
+
+const puppeteer = require('puppeteer');
+
+let browserPromise = puppeteer.launch ({
+    args: [
+    '-no-sandbox'
+    ]
+});
+
+exports.screenshot = async (req, res) => {
+
+    const url = req.query.url || 'http://example.com';
+
+    const browser = await browserPromise;
+    const context = await browser.createIncognitoBrowserContext();
+    const page = await context.newPage();
+    await page.goto(url);
+    const image = await page.screenshot();
+    res.setHeader('Content-Type', 'image/png');
+    res.send(image);
+    context.close();
+
+};
