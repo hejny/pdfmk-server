a3946b3dec8b868a9d677d1161afd5abf1a55ef8
Pavol Hejny <me@pavolhejny.com>
2021-06-02 19:10:22 +0200

    Cleanup of the config

--- src/config.ts
+++ src/config.ts
@@ -13,7 +13,7 @@ const config = ConfigChecker.from(process.env);
 export const SELF_URL = config
     .get('SELF_URL')
     .url()
-    .required()
+    .default(new URL('http://localhost:8080/'))
     .value.toString()
     .replace(/\/+$/g, '');
 
@@ -45,15 +45,4 @@ export const PORT = config
     .get('PORT')
     .number()
     .default(3000).value!;
-*/
-
-/*
-export const CACHE_DIR = config.get('CACHE_DIR').required().value;
-
-
-// TODO: Choise between local and AWS S3 cache
-export const AWS_S3_BUCKET_NAME = config.get('AWS_S3_BUCKET_NAME').default('untitled').value!;
-export const AWS_ACCESS_KEY_ID = config.get('AWS_ACCESS_KEY_ID').value;
-export const AWS_SECRET_ACCESS_KEY = config.get('AWS_SECRET_ACCESS_KEY').value;
-export const AWS_REGION = config.get('AWS_REGION').value;
 */
